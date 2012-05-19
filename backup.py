f7df212ca48d87785bad5916a494bacbeecb6d47
Matt Kenney <mkenney@mjk-tech.com>
2012-05-19 14:23:58 +0000

    add script to backup redis database to s3

--- backup.py
+++ backup.py
@@ -17,17 +17,21 @@
 # You should have received a copy of the GNU Affero General Public License
 # along with Feedsquish.  If not, see <http://www.gnu.org/licenses/>.
 #
+import datetime
+
 import boto
 
 filename = '/var/lib/redis/dump.rdb'
 
 def backup():
+    now = datetime.datetime.utcnow()
+    prefix = now.strftime('%w-%H')
     config = boto.pyami.config.Config()
     bucket_name = config.get('Redis', 'bucket')
     conn = boto.connect_s3()
     bucket = conn.get_bucket(bucket_name)
     key = boto.s3.key.Key(bucket)
-    key.key = filename
+    key.key = prefix + '/' + filename
     key.set_contents_from_filename(filename)
 
 if __name__ == '__main__':
