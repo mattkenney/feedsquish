5b8ef981f896a55dac70ab03fa969428008b491c
Matt Kenney <mkenney@mjk-tech.com>
2016-03-31 02:19:13 +0000

    update CSS, swipe to hide&next

---
+++ static_src/js/script.js
@@ -0,0 +1,30 @@
+(function(){
+
+var x, threshold = 150;
+
+function start(evt) {
+  x = evt.touches[0].clientX;
+}
+
+function move (evt) {
+  if (typeof x === 'undefined') return;
+  var delta = evt.touches[0].clientX - x;
+  if (delta > threshold) {
+    cancel();
+  } else if (delta < -threshold) {
+    cancel();
+    document.getElementById('hide-next').click();
+  }
+}
+
+function cancel() {
+  x = undefined;
+}
+
+document.addEventListener('touchstart', start);
+document.addEventListener('touchmove', move);
+document.addEventListener('touchcancel', cancel);
+document.addEventListener('touchend', cancel);
+document.getElementById('hide-next').focus();
+
+})();
